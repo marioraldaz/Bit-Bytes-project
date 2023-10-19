144f35e1004972a240cfa9ca73f6f9101d14fba0
marioraldaz <marioraldaz@gmail.com>
2023-10-19 13:16:53 +0200

    Added components

---
+++ src/components/RAMs/RAMs.svelte
@@ -0,0 +1,20 @@
+<script>
+    import Product from "src/body/Product.svelte";
+    import RAMs from "src/components/RAMs/RAMs.json";
+    let arrayRams = RAMs;
+</script>
+
+
+<div class="container">
+    {#each arrayRams as RAM}
+        <Product componentName:{RAM.name} id:{RAM.name} 
+        image:{RAM.logo} specifications:{RAM.specifications} description:{RAM.description}/>
+    {/each}
+</div>
+
+
+
+<style lang="scss">
+
+
+</style>
