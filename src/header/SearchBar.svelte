fd54c82edc783adc6f5263ed1f133a938ed4c092
Oscar <oskr234@gmail.com>
2023-10-25 10:37:52 +0200

    Fixed img of the search bar

--- src/header/SearchBar.svelte
+++ src/header/SearchBar.svelte
@@ -1,12 +1,12 @@
 <script>
-  import products from "../components/products.json"
+  import products from "../components/products.json";
 
   let arrayProducts = products.products;
-  let output=[];
+  let output = [];
 
   function searchProducts(userInput) {
-    for(var i = 0; i < arrayProducts.length; i++){
-      if(arrayProducts[i].name.match(userInput)){
+    for (var i = 0; i < arrayProducts.length; i++) {
+      if (arrayProducts[i].name.match(userInput)) {
         output.push(arrayProducts[i].name);
       }
     }
@@ -16,34 +16,37 @@
 </script>
 
 <div class="container">
-  <input type="search" class="container__searchBar" placeholder="   Search for amazing components" bind:value={userInput} />
+  <input
+    type="search"
+    class="container__searchBar"
+    placeholder="   Search for amazing components"
+    bind:value={userInput}
+  />
   <button class="container__search-button" on:click={searchProducts}>
     <div class="container__search-button__div">
-      <img alt="logo" src="src/images/usuario.png"/>
+      <img alt="logo" src="../images/usuario.png" />
     </div>
   </button>
-   <div class="container__results">
+  <div class="container__results">
     <h1>Results:</h1>
     {#key output}
-    {#each output as item, index (item.id)}
-      <div class="container__result__elem"><p>dasdasda</p></div>
-    {/each}
+      {#each output as item, index (item.id)}
+        <div class="container__result__elem"><p>dasdasda</p></div>
+      {/each}
     {/key}
   </div>
 </div>
 
- 
-
 <style lang="scss">
   .container {
     float: right;
-    width:40rem;
+    width: 40rem;
     height: 4.5rem;
     transform: translate(0%, 50%);
 
-    &__result{
-      &__elem{
-        float:inline-end;
+    &__result {
+      &__elem {
+        float: inline-end;
       }
     }
     &__searchBar {
@@ -58,19 +61,21 @@
       &:focus {
         border: 0.3rem px solid violet;
         outline: none;
-      } 
+      }
     }
-    &__search-button{
-        float:right;
-        width:2rem;
-        height: 2rem;
-        &__div{
-        
-        width:100%;
+    &__search-button {
+      float: right;
+      width: 5rem;
+      height: 5rem;
+      &__div {
+        width: 100%;
         height: 100%;
-        background-color: red;
+      
+        & img{
+          height: 100%;
         }
       }
-  }
 
+    }
+  }
 </style>
